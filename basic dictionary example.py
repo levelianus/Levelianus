ae18597fa08fb7eec78161180ef055b306057a0e
levelianus <levelianusdidace16@gmail.com>
2025-06-18 15:26:17 +0300

    Create basic dictionary example.py

---
+++ basic dictionary example.py
@@ -0,0 +1,7 @@
+student = ["name": "alice", "age": 20' "grade": "a"]
+"name", "age", "grade" are keys
+"alice", 20, "a" are values
+accessing values:
+st.write(student["name"])
+st.write(student["age"])
+st.write(student["grade"])
